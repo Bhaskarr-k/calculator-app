b8418de4989a5656f598db6be45325d109a411c8
Bhaskar <bhaskarkotakonda123@gmail.com>
2025-02-11 09:46:03 +0530

    not completed

---
+++ script.js
@@ -0,0 +1,10 @@
+const display = document.querySelectorAll("#result")
+const buttons = document.querySelectorAll('button')
+
+
+buttons.forEach(function(btn){
+    btn.addEventListener('click',function(){
+        console.log('is working')
+        const valur
+    })
+})
